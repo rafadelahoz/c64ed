a1bdd349aceac1e467964418840d18c4d38f6ae1
Rafa de la Hoz <rafa.delahoz@gmail.com>
2020-02-03 17:37:09 +0100

    feat(palette): started drafting palette picker

---
+++ js/palette.js
@@ -0,0 +1,356 @@
+
+var black   = ['#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000'];
+var white   = ['#202020', '#404040', '#606060', '#808080', '#9f9f9f', '#bfbfbf', '#dfdfdf', '#ffffff'];
+var red     = ['#580902', '#782922', '#984942', '#b86962', '#d88882', '#f7a8a2', '#ffc8c2', '#ffe8e2'];
+var cyan    = ['#00373d', '#08575d', '#27777d', '#47969d', '#67b6bd', '#87d6dd', '#a7f6fd', '#c7ffff'];
+var purple  = ['#4b0056', '#6b1f76', '#8b3f96', '#aa5fb6', '#ca7fd6', '#ea9ff6', '#ffbfff', '#ffbfff'];
+var green   = ['#004000', '#156009', '#358029', '#55a049', '#74c069', '#94e089', '#b4ffa9', '#d4ffc9'];
+var blue    = ['#20116d', '#40318d', '#6051ac', '#8071cc', '#9f90ec', '#bfb0ff', '#dfd0ff', '#fff0ff'];
+var yellow  = ['#202f00', '#404f00', '#606f13', '#808e33', '#9fae53', '#bfce72', '#dfee92', '#ffffb2'];
+var orange  = ['#4b1500', '#6b3409', '#8b5429', '#aa7449', '#ca9469', '#eab489', '#ffd4a9', '#fff4c9'];
+var orange  = ['', '', '', '', '', '', '', ''];
+var orange  = ['', '', '', '', '', '', '', ''];
+
+module.exports = {
+
+};
+
+<tbody id="dodo">
+
+
+<tr>
+<td>0 — black</td>
+<td>0</td>
+<td>0
+</td>
+<td style="color:white; background:#000;"><u>0,0</u>
+</td>
+<td style="color:white; background:#000;">0,1
+</td>
+<td style="color:white; background:#000;">0,2
+</td>
+<td style="color:white; background:#000;">0,3
+</td>
+<td style="color:white; background:#000;">0,4
+</td>
+<td style="color:white; background:#000;">0,5
+</td>
+<td style="color:white; background:#000;">0,6
+</td>
+<td style="color:white; background:#000;">0,7
+</td></tr>
+<tr>
+<td>1 — white</td>
+<td>0</td>
+<td>0
+</td>
+<td style="color:white; background:#202020;">1,0
+</td>
+<td style="color:white; background:#404040;">1,1
+</td>
+<td style="color:white; background:#606060;">1,2
+</td>
+<td style="color:white; background:gray;">1,3
+</td>
+<td style="color:white; background:#9f9f9f;">1,4
+</td>
+<td style="color:black; background:#bfbfbf;">1,5
+</td>
+<td style="color:black; background:#dfdfdf;">1,6
+</td>
+<td style="color:black; background:#fff;"><u>1,7</u>
+</td></tr>
+<tr>
+<td>2 — red</td>
+<td>−0.3826834</td>
+<td>0.9238795
+</td>
+<td style="color:white; background:#580902;">2,0
+</td>
+<td style="color:white; background:#782922;">2,1
+</td>
+<td style="color:white; background:#984942;">2,2
+</td>
+<td style="color:white; background:#b86962;"><u>2,3</u>
+</td>
+<td style="color:white; background:#d88882;">2,4
+</td>
+<td style="color:black; background:#f7a8a2;">2,5
+</td>
+<td style="color:black; background:#ffc8c2;">2,6*
+</td>
+<td style="color:black; background:#ffe8e2;">2,7*
+</td></tr>
+<tr>
+<td>3 — cyan</td>
+<td>0.3826834</td>
+<td>−0.9238795
+</td>
+<td style="color:white; background:#00373d;">3,0*
+</td>
+<td style="color:white;background: #08575d;">3,1
+</td>
+<td style="color:white; background:#27777d;">3,2
+</td>
+<td style="color:white; background:#47969d;">3,3
+</td>
+<td style="color:white; background:#67b6bd;">3,4
+</td>
+<td style="color:black; background:#87d6dd;">3,5
+</td>
+<td style="color:black; background:#a7f6fd;"><u>3,6</u>
+</td>
+<td style="color:black; background:#c7ffff;">3,7*
+</td></tr>
+<tr>
+<td>4 — purple</td>
+<td>0.7071068</td>
+<td>0.7071068
+</td>
+<td style="color:white; background:#4b0056;">4,0*
+</td>
+<td style="color:white; background:#6b1f76;">4,1
+</td>
+<td style="color:white; background:#8b3f96;">4,2
+</td>
+<td style="color:white; background:#aa5fb6;">4,3
+</td>
+<td style="color:white; background:#ca7fd6;"><u>4,4</u>
+</td>
+<td style="color:black; background:#ea9ff6;">4,5
+</td>
+<td style="color:black; background:#ffbfff;">4,6*
+</td>
+<td style="color:black; background:#ffdfff;">4,7*
+</td></tr>
+<tr>
+<td>5 — green</td>
+<td>−0.7071068</td>
+<td>−0.7071068
+</td>
+<td style="color:white; background:#004000;">5,0*
+</td>
+<td style="color:white; background:#156009;">5,1
+</td>
+<td style="color:white; background:#358029;">5,2
+</td>
+<td style="color:white; background:#55a049;"><u>5,3</u>
+</td>
+<td style="color:white; background:#74c069;">5,4
+</td>
+<td style="color:black; background:#94e089;">5,5
+</td>
+<td style="color:black; background:#b4ffa9;">5,6*
+</td>
+<td style="color:black; background:#d4ffc9;">5,7*
+</td></tr>
+<tr>
+<td>6 — blue</td>
+<td>1</td>
+<td>0
+</td>
+<td style="color:white; background:#20116d;">6,0
+</td>
+<td style="color:white; background:#40318d;">6,1
+</td>
+<td style="color:white; background:#6051ac;">6,2
+</td>
+<td style="color:white; background:#8071cc;">6,3
+</td>
+<td style="color:white; background:#9f90ec;"><u>6,4</u>
+</td>
+<td style="color:black; background:#bfb0ff;">6,5*
+</td>
+<td style="color:black; background:#dfd0ff;">6,6*
+</td>
+<td style="color:black; background:#fff0ff;">6,7*
+</td></tr>
+<tr>
+<td>7 — yellow</td>
+<td>−1</td>
+<td>0
+</td>
+<td style="color:white; background:#202f00;">7,0*
+</td>
+<td style="color:white; background:#404f00;">7,1*
+</td>
+<td style="color:white; background:#606f13;">7,2
+</td>
+<td style="color:white; background:#808e33;">7,3
+</td>
+<td style="color:white; background:#9fae53;">7,4
+</td>
+<td style="color:black; background:#bfce72;">7,5
+</td>
+<td style="color:black; background:#dfee92;">7,6
+</td>
+<td style="color:black; background:#ffffb2;"><u>7,7</u>*
+</td></tr>
+<tr>
+<td>8 — orange</td>
+<td>−0.7071068</td>
+<td>0.7071068
+</td>
+<td style="color:white; background:#4b1500;">8,0*
+</td>
+<td style="color:white; background:#6b3409;">8,1
+</td>
+<td style="color:white; background:#8b5429;">8,2
+</td>
+<td style="color:white; background:#aa7449;">8,3
+</td>
+<td style="color:white; background:#ca9469;">8,4
+</td>
+<td style="color:black; background:#eab489;">8,5
+</td>
+<td style="color:black; background:#ffd4a9;">8,6*
+</td>
+<td style="color:black; background:#fff4c9;">8,7*
+</td></tr>
+<tr>
+<td>9 — brown</td>
+<td>−0.9238795</td>
+<td>0.3826834
+</td>
+<td style="color:white; background:#372200;">9,0*
+</td>
+<td style="color:white; background:#574200;">9,1*
+</td>
+<td style="color:white; background:#776219;">9,2
+</td>
+<td style="color:white; background:#978139;">9,3
+</td>
+<td style="color:white; background:#b7a158;">9,4
+</td>
+<td style="color:black; background:#d7c178;">9,5
+</td>
+<td style="color:black; background:#f6e198;">9,6
+</td>
+<td style="color:black; background:#ffffb8;">9,7*
+</td></tr>
+<tr>
+<td>10 — yellow-green</td>
+<td>−0.9238795</td>
+<td>−0.3826834
+</td>
+<td style="color:white; background:#093a00;">10,0*
+</td>
+<td style="color:white; background:#285900;">10,1*
+</td>
+<td style="color:white; background:#487919;">10,2
+</td>
+<td style="color:white; background:#689939;">10,3
+</td>
+<td style="color:white; background:#88b958;">10,4
+</td>
+<td style="color:black; background:#a8d978;">10,5
+</td>
+<td style="color:black; background:#c8f998;">10,6
+</td>
+<td style="color:black; background:#e8ffb8;">10,7*
+</td></tr>
+<tr>
+<td>11 — pink</td>
+<td>0</td>
+<td>1
+</td>
+<td style="color:white; background:#5d0120;">11,0
+</td>
+<td style="color:white; background:#7d2140;">11,1
+</td>
+<td style="color:white; background:#9c4160;">11,2
+</td>
+<td style="color:white; background:#bc6180;">11,3
+</td>
+<td style="color:white; background:#dc809f;">11,4
+</td>
+<td style="color:black; background:#fca0bf;">11,5
+</td>
+<td style="color:black; background:#ffc0df;">11,6*
+</td>
+<td style="color:black; background:#ffe0ff;">11,7*
+</td></tr>
+<tr>
+<td>12 — blue-green</td>
+<td>0</td>
+<td>−1
+</td>
+<td style="color:white; background:#003f20;">12,0*
+</td>
+<td style="color:white; background:#035f40;">12,1
+</td>
+<td style="color:white; background:#237f60;">12,2
+</td>
+<td style="color:white; background:#439e80;">12,3
+</td>
+<td style="color:white; background:#63be9f;">12,4
+</td>
+<td style="color:black; background:#82debf;"><u>12,5</u>
+</td>
+<td style="color:black; background:#a2fedf;">12,6
+</td>
+<td style="color:black; background:#c2ffff;">12,7*
+</td></tr>
+<tr>
+<td>13 — light blue</td>
+<td>0.7071068</td>
+<td>−0.7071068
+</td>
+<td style="color:white; background:#002b56;">13,0*
+</td>
+<td style="color:white; background:#154b76;">13,1
+</td>
+<td style="color:white; background:#356b96;">13,2
+</td>
+<td style="color:white; background:#558bb6;">13,3
+</td>
+<td style="color:white; background:#74abd6;">13,4
+</td>
+<td style="color:black; background:#94cbf6;">13,5
+</td>
+<td style="color:black; background:#b4eaff;"><u>13,6</u>*
+</td>
+<td style="color:black; background:#d4ffff;">13,7*
+</td></tr>
+<tr>
+<td>14 — dark blue</td>
+<td>0.9238795</td>
+<td>0.3826834
+</td>
+<td style="color:white; background:#370667;">14,0
+</td>
+<td style="color:white; background:#572687;">14,1
+</td>
+<td style="color:white; background:#7746a7;"><u>14,2</u>
+</td>
+<td style="color:white; background:#9766c6;">14,3
+</td>
+<td style="color:white; background:#b786e6;">14,4
+</td>
+<td style="color:black; background:#d7a6ff;">14,5*
+</td>
+<td style="color:black; background:#f6c5ff;">14,6*
+</td>
+<td style="color:black; background:#ffe5ff;">14,7*
+</td></tr>
+<tr>
+<td>15 — light green</td>
+<td>−0.3826834</td>
+<td>−0.9238795
+</td>
+<td style="color:white; background:#004202;">15,0*
+</td>
+<td style="color:white; background:#086222;">15,1
+</td>
+<td style="color:white; background:#278242;">15,2
+</td>
+<td style="color:white; background:#47a262;">15,3
+</td>
+<td style="color:white; background:#67c282;">15,4
+</td>
+<td style="color:black; background:#87e2a2;"><u>15,5</u>
+</td>
+<td style="color:black; background:#a7ffc2;">15,6*
+</td>
+<td style="color:black; background:#c7ffe2;">15,7*
+</td></tr></tbody>
